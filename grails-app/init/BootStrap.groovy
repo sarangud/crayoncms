d93d0dde40f8d19b57e257508eb3ccb83040bd9a
goGula Krishnan <rgogulakrishnan@gmail.com>
2016-09-16 22:30:37 +0530

    adding menu in bootstrap

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -128,8 +128,6 @@ class BootStrap {
 </section>''', access: "Public"
             ).save()
 
-            println Page.list()
-
             new Block(
                    name: "Tweets", slug: "tweets", content: '''
 <section class="4u 12u(mobile)">
@@ -254,12 +252,36 @@ class BootStrap {
 			
 			Menu.findOrSaveWhere(name: "Home", menuType: MenuType.PAGE, menuTypeValue: "/home", menuGroup: header, 
 				position: 1, parent:0, cssClass: "", access: "", targetBlank: false)
-			Menu.findOrSaveWhere(name: "About", menuType: MenuType.PAGE, menuTypeValue: "/about", menuGroup: header, 
+			Menu.findOrSaveWhere(name: "Dropdown", menuType: MenuType.PAGE, menuTypeValue: "#", menuGroup: header, 
 				position: 2, parent:0, cssClass: "", access: "", targetBlank: false)
-			Menu.findOrSaveWhere(name: "Conact", menuType: MenuType.PAGE, menuTypeValue: "/contact", menuGroup: header, 
-				position: 0, parent:2, cssClass: "", access: "", targetBlank: false)
-			Menu.findOrSaveWhere(name: "Work", menuType: MenuType.PAGE, menuTypeValue: "/work", menuGroup: header, 
-				position: 3, parent: 0, cssClass: "", access: "", targetBlank: false)
+			Menu.findOrSaveWhere(name: "Right Sidebar", menuType: MenuType.PAGE, menuTypeValue: "/right-sidebar", menuGroup: header, 
+				position: 3, parent:0, cssClass: "", access: "", targetBlank: false)
+			Menu.findOrSaveWhere(name: "About", menuType: MenuType.PAGE, menuTypeValue: "/about", menuGroup: header, 
+				position: 4, parent: 0, cssClass: "", access: "", targetBlank: false)
+			Menu.findOrSaveWhere(name: "Login", menuType: MenuType.PAGE, menuTypeValue: "/login", menuGroup: header,
+				position: 5, parent: 0, cssClass: "", access: "", targetBlank: false)
+			Menu.findOrSaveWhere(name: "Admin", menuType: MenuType.PAGE, menuTypeValue: "/page", menuGroup: header,
+				position: 6, parent: 0, cssClass: "", access: "", targetBlank: false)
+			Menu.findOrSaveWhere(name: "Logout", menuType: MenuType.PAGE, menuTypeValue: "/logout", menuGroup: header,
+				position: 7, parent: 0, cssClass: "", access: "", targetBlank: false)
+			
+			Menu.findOrSaveWhere(name: "Lorem ipsum dolor", menuType: MenuType.PAGE, menuTypeValue: "#", menuGroup: header,
+				position: 1, parent: 2, cssClass: "", access: "", targetBlank: false)
+			Menu.findOrSaveWhere(name: "Magna phasellus", menuType: MenuType.PAGE, menuTypeValue: "#", menuGroup: header,
+				position: 2, parent: 2, cssClass: "", access: "", targetBlank: false)
+			Menu.findOrSaveWhere(name: "Etiam dolore nisl", menuType: MenuType.PAGE, menuTypeValue: "#", menuGroup: header,
+				position: 3, parent: 2, cssClass: "", access: "", targetBlank: false)
+			Menu.findOrSaveWhere(name: "And a submenu &hellip;", menuType: MenuType.PAGE, menuTypeValue: "#", menuGroup: header,
+				position: 4, parent: 2, cssClass: "", access: "", targetBlank: false)
+			Menu.findOrSaveWhere(name: "Veroeros feugiat", menuType: MenuType.PAGE, menuTypeValue: "#", menuGroup: header,
+				position: 5, parent: 2, cssClass: "", access: "", targetBlank: false)
+			
+			Menu.findOrSaveWhere(name: "Lorem ipsum dolor", menuType: MenuType.PAGE, menuTypeValue: "#", menuGroup: header,
+				position: 1, parent: 11, cssClass: "", access: "", targetBlank: false)
+			Menu.findOrSaveWhere(name: "Magna phasellus", menuType: MenuType.PAGE, menuTypeValue: "#", menuGroup: header,
+				position: 2, parent: 11, cssClass: "", access: "", targetBlank: false)
+			Menu.findOrSaveWhere(name: "Phasellus consequat", menuType: MenuType.PAGE, menuTypeValue: "#", menuGroup: header,
+				position: 3, parent: 11, cssClass: "", access: "", targetBlank: false)
 
         } catch(e) {
             println e
